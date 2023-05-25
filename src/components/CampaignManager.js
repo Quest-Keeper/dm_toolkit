86c2379db04134d28100ef2bffb142e5e32d4c9c
Harrison Ryan <hwryan12@gmail.com>
2023-05-25 12:23:07 -0500

    feature: Add campaign manager functionality

--- src/components/CampaignManager.js
+++ src/components/CampaignManager.js
@@ -1,19 +1,120 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Card, Button, Form, Accordion, Badge, ListGroup } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../SharedStyles.css';
 
 const CampaignManager = () => {
+  const [campaigns, setCampaigns] = useState([]);
+  const [campaignName, setCampaignName] = useState('');
+  const [sessionName, setSessionName] = useState('');
+  const [noteContent, setNoteContent] = useState('');
+  const [selectedCampaign, setSelectedCampaign] = useState(null);
+  const [selectedSession, setSelectedSession] = useState(null);
+
+  const addCampaign = () => {
+    const newCampaign = {
+      id: new Date().getTime(),
+      name: campaignName,
+      sessions: []
+    };
+
+    setCampaigns([newCampaign, ...campaigns]);
+    setCampaignName('');
+  };
+
+  const addSession = (campaignId) => {
+    const newSession = {
+      id: new Date().getTime(),
+      name: sessionName,
+      notes: []
+    };
+
+    setCampaigns(campaigns.map(campaign => campaign.id === campaignId ? {...campaign, sessions: [newSession, ...campaign.sessions]} : campaign));
+    setSessionName('');
+  };
+
+  const addNote = (campaignId, sessionId) => {
+    const newNote = {
+      id: new Date().getTime(),
+      content: noteContent
+    };
+
+    setCampaigns(campaigns.map(campaign => campaign.id === campaignId ? {...campaign, sessions: campaign.sessions.map(session => session.id === sessionId ? {...session, notes: [newNote, ...session.notes]} : session)} : campaign));
+    setNoteContent('');
+  };
+
   return (
-    <div className="col-md-6 offset-md-3">
-      <div className="card bg-warning text-white mb-3 shadow-lg campaign-card">
-        <div className="card-body text-center">
-          <h5 className="card-title">Campaign Manager</h5>
-          <p className="card-text">
-            Organize your campaigns, sessions, and notes in one convenient place.
-          </p>
-          <a href="#" className="btn btn-light btn-lg campaign-button">Manage Now</a>
-        </div>
-      </div>
-    </div>
+    <Card className="col-md-6 offset-md-3 card bg-dark text-white mb-3 shadow-lg">
+      <Card.Header as="h5">Campaign Manager</Card.Header>
+      <Card.Body>
+        <Form inline className="justify-content-center mb-3">
+          <Form.Control 
+            type="text" 
+            value={campaignName} 
+            onChange={(e) => setCampaignName(e.target.value)} 
+            placeholder="New Campaign"
+            className="mr-2"
+          />
+          <Button variant="outline-light" onClick={addCampaign}>Add Campaign</Button>
+        </Form>
+
+        <Accordion>
+          {campaigns.map(campaign => (
+            <Card key={campaign.id} className="bg-dark text-white">
+              <Accordion.Toggle as={Card.Header} eventKey={campaign.id.toString()}>
+                {campaign.name}
+              </Accordion.Toggle>
+
+              <Accordion.Collapse eventKey={campaign.id.toString()}>
+                <>
+                  <Form inline className="justify-content-center my-3">
+                    <Form.Control 
+                      type="text" 
+                      value={sessionName} 
+                      onChange={(e) => setSessionName(e.target.value)} 
+                      placeholder="New Session"
+                      className="mr-2"
+                    />
+                    <Button variant="outline-light" onClick={() => addSession(campaign.id)}>Add Session</Button>
+                  </Form>
+
+                  {campaign.sessions.map(session => (
+                    <ListGroup variant="flush" key={session.id}>
+                      <ListGroup.Item className="bg-dark text-white">
+                        {session.name}
+                        <Badge pill variant="light" className="ml-2" onClick={() => setSelectedSession(session.id === selectedSession ? null : session.id)}>
+                          Notes
+                        </Badge>
+                      </ListGroup.Item>
+
+                      {session.id === selectedSession && (
+                        <>
+                          <Form className="justify-content-center my-3 mx-5">
+                            <Form.Control 
+                              as="textarea" 
+                              value={noteContent} 
+                              onChange={(e) => setNoteContent(e.target.value)} 
+                              placeholder="New Note"
+                            />
+                            <Button variant="outline-light" className="mt-2" block onClick={() => addNote(campaign.id, session.id)}>Add Note</Button>
+                          </Form>
+
+                          {session.notes.map(note => (
+                            <ListGroup.Item key={note.id} className="bg-dark text-white">
+                              {note.content}
+                            </ListGroup.Item>
+                          ))}
+                        </>
+                      )}
+                    </ListGroup>
+                  ))}
+                </>
+              </Accordion.Collapse>
+            </Card>
+          ))}
+        </Accordion>
+      </Card.Body>
+    </Card>
   );
 };
 
