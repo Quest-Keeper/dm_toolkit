103ae4694209ab764dc81c337944346a3f177bd4
Harrison Ryan <hwryan12@gmail.com>
2023-05-27 08:47:00 -0500

    feature: Implement logic to hit rails server and render content

--- src/components/LootGenerator.js
+++ src/components/LootGenerator.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import '../SharedStyles.css';
 
 const LootGenerator = () => {
+  const [amount, setAmount] = useState(1);
+  const [type, setType] = useState('magic');
+  const [rarity, setRarity] = useState('common');
+  const [loot, setLoot] = useState(null);
+
+  const generateLoot = async () => {
+    try {
+      const response = await axios.get('http://localhost:3001/api/v1/loot', {
+        params: { amount, type, rarity },
+      });
+      setLoot(response.data.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className="col-md-6 offset-md-3">
       <div className="card bg-dark text-white mb-3 shadow-lg loot-card">
@@ -10,7 +27,40 @@ const LootGenerator = () => {
           <p className="card-text">
             Discover valuable treasures and magical items to reward your adventurers.
           </p>
-          <a href="#" className="btn btn-light btn-lg loot-btn">Generate Now</a>
+
+          <select onChange={(e) => setAmount(e.target.value)}>
+            {[...Array(20)].map((_, i) => <option value={i + 1} key={i + 1}>{i + 1}</option>)}
+          </select>
+          
+          <select onChange={(e) => setType(e.target.value)}>
+            <option value="magic">Magic</option>
+            <option value="non-magic">Non-Magic</option>
+            <option value="mixed">Mixed</option>
+          </select>
+
+          <select onChange={(e) => setRarity(e.target.value)}>
+            <option value="common">Common</option>
+            <option value="uncommon">Uncommon</option>
+            <option value="rare">Rare</option>
+            <option value="very rare">Very Rare</option>
+            <option value="legendary">Legendary</option>
+            <option value="artifact">Artifact</option>
+          </select>
+
+          <button onClick={generateLoot} className="btn btn-light btn-lg loot-btn">Generate Now</button>
+          {loot && loot.map((item, index) => (
+            <div key={index}>
+              <h2>{item.name}</h2>
+              {item.description && <p>Description: {item.description}</p>}
+              {item.level && <p>Level: {item.level}</p>}
+              {item.uses && <p>Uses: {item.uses}</p>}
+              {item.category && <p>Category: {item.category}</p>}
+              {item.damage_dice && <p>Damage Dice: {item.damage_dice}</p>}
+              {item.damage_type && <p>Damage Type: {item.damage_type}</p>}
+              {item.rarity && <p>Rarity: {item.rarity}</p>}
+              {item.ac && <p>AC: {item.ac}</p>}
+            </div>
+          ))}
         </div>
       </div>
     </div>
