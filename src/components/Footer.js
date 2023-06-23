b05d2535f37b6ea33ff1f2bbcf2b2778c754b0ff
Harrison Ryan <hwryan12@gmail.com>
2023-06-22 20:49:14 -0500

    docs: Update name in footer/header

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="footer mt-auto py-3 bg-dark text-white">
       <div className="container-fluid d-flex justify-content-center">
-        <span>&copy; 2023 My D&D App. All rights reserved.</span>
+        <span>&copy; 2023 Quest Keeper. All rights reserved.</span>
       </div>
     </footer>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
   return (
     <header className="p-1 bg-dark text-white text-center">
-      <h1 className="display-4">Welcome to My D&D App</h1>
+      <h1 className="display-4">Welcome to Quest Keeper</h1>
       <p className="lead">Unleash your imagination and embark on epic adventures!</p>
     </header>
   );
